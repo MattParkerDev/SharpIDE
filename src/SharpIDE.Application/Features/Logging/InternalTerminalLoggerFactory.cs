24b69da05124d0ceab9a4e51fc76e659592e70c8
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-11-13 18:37:36 +1000

    Update InternalTerminalLoggerFactory.cs

--- src/SharpIDE.Application/Features/Logging/InternalTerminalLoggerFactory.cs
+++ src/SharpIDE.Application/Features/Logging/InternalTerminalLoggerFactory.cs
@@ -14,19 +14,12 @@ public class InternalTerminalLoggerFactory
 
 	private static ILogger CreateLogger(string parameters, LoggerVerbosity loggerVerbosity, TextWriter output)
 	{
-		string[]? args = [];
-		bool supportsAnsi = true;
-		bool outputIsScreen = true;
-		uint? originalConsoleMode = 0x0007;
-
-		//var logger = new TerminalLogger(loggerVerbosity, originalConsoleMode);
+		//var logger = TerminalLogger.CreateTerminalOrConsoleLogger(args, supportsAnsi, outputIsScreen, originalConsoleMode);
 		var terminal = new Terminal(output);
 		var logger = new TerminalLogger(terminal);
 		var field = typeof(TerminalLogger).GetField("_manualRefresh", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 		Guard.Against.Null(field, nameof(field));
 		field.SetValue(logger, false);
-
-		//var logger = TerminalLogger.CreateTerminalOrConsoleLogger(args, supportsAnsi, outputIsScreen, originalConsoleMode);
 
 		logger.Parameters = parameters;
 		logger.Verbosity = loggerVerbosity;
