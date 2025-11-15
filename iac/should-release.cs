1477913b11291364983cd14589092a465bc48be9
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-11-15 16:08:05 +1000

    use version file

--- iac/should-release.cs
+++ iac/should-release.cs
@@ -3,10 +3,15 @@
 using Octokit;
 using NuGet.Versioning;
 
-var github = new GitHubClient(new ProductHeaderValue("SharpIDE-CI"));
-var version = NuGetVersion.Parse("0.1.2");
+var versionFile = new FileInfo(Path.Combine(GetGitRootPath(), "src", "SharpIDE.Godot", "version.txt"));
+if (versionFile.Exists is false) throw new FileNotFoundException(versionFile.FullName);
+var versionText = await File.ReadAllTextAsync(versionFile.FullName);
+
+var version = NuGetVersion.Parse(versionText);
 var versionString = version.ToNormalizedString();
 var releaseTag = $"v{versionString}";
+
+var github = new GitHubClient(new ProductHeaderValue("SharpIDE-CI"));
 var owner = "MattParkerDev";
 var repo = "SharpIDE";
 var release = await GetReleaseOrNull();
@@ -26,4 +31,20 @@ async Task<Release?> GetReleaseOrNull()
 	{
 		return null;
 	}
+}
+
+static string GetGitRootPath()
+{
+	var currentDirectory = Directory.GetCurrentDirectory();
+	var gitRoot = currentDirectory;
+	while (!Directory.Exists(Path.Combine(gitRoot, ".git")))
+	{
+		gitRoot = Path.GetDirectoryName(gitRoot); // parent directory
+		if (string.IsNullOrWhiteSpace(gitRoot))
+		{
+			throw new Exception("Could not find git root");
+		}
+	}
+
+	return gitRoot;
 }
