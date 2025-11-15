2f141a70e665cac1ca8ad7f9d92a37c27289eea5
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-11-15 15:41:28 +1000

    asdf

--- iac/should-release.cs
+++ iac/should-release.cs
@@ -1,5 +1,29 @@
+#:package Octokit
+#:package NuGet.Versioning
+using Octokit;
+using NuGet.Versioning;
 
+var github = new GitHubClient(new ProductHeaderValue("SharpIDE-CI"));
+var version = NuGetVersion.Parse("0.1.2");
+var versionString = version.ToNormalizedString();
+var releaseTag = $"v{versionString}";
+var owner = "MattParkerDev";
+var repo = "SharpIDE";
+var release = await GetReleaseOrNull();
+var resultString = release is null ? "true" : "false";
 
-// var test = await github.Repository.Release.Get(owner, repo, newRelease.TagName);
-Console.WriteLine("false");
+Console.WriteLine(resultString);
 return 0;
+
+async Task<Release?> GetReleaseOrNull()
+{
+	try
+	{
+		var release = await github.Repository.Release.Get(owner, repo, releaseTag);
+		return release;
+	}
+	catch (NotFoundException)
+	{
+		return null;
+	}
+}
