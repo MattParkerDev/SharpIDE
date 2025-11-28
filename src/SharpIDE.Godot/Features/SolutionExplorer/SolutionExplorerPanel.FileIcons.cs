1a8328260d346526f18e31263d3b98158c0fb0d4
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-11-29 00:29:43 +1000

    add css icon

--- src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
+++ src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
@@ -10,6 +10,7 @@ public partial class SolutionExplorerPanel
     private readonly Texture2D _jsonIcon = ResourceLoader.Load<Texture2D>("uid://csrwpjk77r731");
     private readonly Texture2D _jsIcon = ResourceLoader.Load<Texture2D>("uid://cpdobpjrm2suc");
     private readonly Texture2D _htmlIcon = ResourceLoader.Load<Texture2D>("uid://q0cktiwdkt1e");
+    private readonly Texture2D _cssIcon = ResourceLoader.Load<Texture2D>("uid://b6m4rm5u8hd1c");
     private readonly Texture2D _txtIcon = ResourceLoader.Load<Texture2D>("uid://b6bpjhs2o1j2l");
 
     private Texture2D GetIconForFileExtension(string fileExtension)
@@ -21,6 +22,7 @@ public partial class SolutionExplorerPanel
             ".json" => _jsonIcon,
             ".js" => _jsIcon,
             ".html" or ".htm" => _htmlIcon,
+            ".css" => _cssIcon,
             ".txt" => _txtIcon,
             _ => _csIcon
         };    
