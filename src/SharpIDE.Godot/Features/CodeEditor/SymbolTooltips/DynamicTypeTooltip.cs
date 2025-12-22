fa26dbc38360bca830b3edaab0d359abbe672c65
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-12-22 12:12:19 +1000

    Update DynamicTypeTooltip.cs

--- src/SharpIDE.Godot/Features/CodeEditor/SymbolTooltips/DynamicTypeTooltip.cs
+++ src/SharpIDE.Godot/Features/CodeEditor/SymbolTooltips/DynamicTypeTooltip.cs
@@ -9,9 +9,11 @@ public static partial class SymbolInfoComponents
     public static RichTextLabel GetDynamicTypeSymbolInfo(IDynamicTypeSymbol symbol)
     {
         var label = new RichTextLabel();
+        label.PushColor(CachedColors.White);
         label.PushFont(MonospaceFont);
         label.PushColor(CachedColors.KeywordBlue);
         label.AddText(symbol.ToDisplayString());
+        label.Pop();
         label.Pop();
         label.Pop();
         return label;
