e451d1258b8b0c2a58fca5034a47d89608da7a26
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-12-22 11:50:44 +1000

    Update MethodTooltip.cs

--- src/SharpIDE.Godot/Features/CodeEditor/SymbolTooltips/MethodTooltip.cs
+++ src/SharpIDE.Godot/Features/CodeEditor/SymbolTooltips/MethodTooltip.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections.Immutable;
-
-using Godot;
+﻿using Godot;
 using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
 
 namespace SharpIDE.Godot.Features.CodeEditor;
 
