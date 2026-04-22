4d8bad39ff39a7293c249e802e074d94c133fc48
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-04-23 00:25:31 +1000

    SlnPicker Windows specific changes

--- src/SharpIDE.Godot/Features/SlnPicker/SlnPickerTitleBar.cs
+++ src/SharpIDE.Godot/Features/SlnPicker/SlnPickerTitleBar.cs
@@ -3,7 +3,7 @@ using Godot;
 namespace SharpIDE.Godot.Features.SlnPicker;
 
 /// ExtendToTitle is currently only implemented on MacOS, and is also currently disabled in the project
-public partial class SlnPickerTitleBar : Control
+public partial class SlnPickerTitleBar : MarginContainer
 {
 	private Control _leftSpacer = null!;
 	private Control _rightSpacer = null!;
@@ -18,7 +18,14 @@ public partial class SlnPickerTitleBar : Control
 			return;
 		}
 		Visible = true;
-		DisplayServer.WindowSetWindowButtonsOffset(new Vector2I(16, 16));
+		if (OS.GetName() == "macOS")
+		{
+			DisplayServer.WindowSetWindowButtonsOffset(new Vector2I(16, 16));
+		}
+		else if (OS.GetName() == "Windows")
+		{
+			DisplayServer.WindowSetWindowButtonsOffset(new Vector2I(46/2, 44/2)); // Taller, like firefox
+		}
 		_leftSpacer = GetNode<Control>("%LeftSpacer");
 		_rightSpacer = GetNode<Control>("%RightSpacer");
 		_window = GetWindow();
@@ -34,18 +41,29 @@ public partial class SlnPickerTitleBar : Control
 
 		// Use safe margins to avoid placing UI under traffic-light buttons
 		// margins.X = left side width used by buttons (LTR)
-		// margins.Y = right side width used by buttons (RTL)
+		// margins.Y = right side width used by buttons (RTL) // or other platforms, e.g. windows
 		// margins.Z = height of the button area
+		// until I run into something different, I am just going to use the zero-ness of X or Y, to mean use the other one as the margin
 		var margins = DisplayServer.WindowGetSafeTitleMargins();
-		_leftSpacer.CustomMinimumSize = new Vector2(margins.X, 0);
-		_rightSpacer.CustomMinimumSize = new Vector2(margins.X, 0);
+		var spacerMinimumSizeX = margins.X is not 0 ? margins.X : margins.Y;
+		_leftSpacer.CustomMinimumSize = new Vector2(spacerMinimumSizeX, margins.Z);
+		_rightSpacer.CustomMinimumSize = new Vector2(spacerMinimumSizeX, margins.Z);
 	}
 
 	public override void _GuiInput(InputEvent @event)
 	{
-		if (@event is InputEventMouseButton { ButtonIndex: MouseButton.Left, Pressed: true })
+		if (@event is InputEventMouseButton { ButtonIndex: MouseButton.Left, Pressed: true } leftClick)
 		{
-			DisplayServer.WindowStartDrag();
+			if (leftClick.DoubleClick)
+			{
+				DisplayServer.WindowSetMode(DisplayServer.WindowGetMode() is DisplayServer.WindowMode.Maximized
+					? DisplayServer.WindowMode.Windowed
+					: DisplayServer.WindowMode.Maximized);
+			}
+			else
+			{
+				DisplayServer.WindowStartDrag();
+			}
 		}
 	}
 	
