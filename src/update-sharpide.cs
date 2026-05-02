4c57727453d343e22b9b55d00c139f9f395c7330
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-05-03 01:49:08 +1000

    update on macos

--- src/update-sharpide.cs
+++ src/update-sharpide.cs
@@ -10,6 +10,13 @@ try
 	var sharpIdeRunningExecutableFilePath = args.ElementAtOrDefault(1);
 	var newSharpIdeReleaseFilePath = args.ElementAtOrDefault(2);
 	var runningIdeInstancePidString = args.ElementAtOrDefault(3);
+
+	var logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SharpIDE", "update-log.txt");
+	var logFileStream = new FileStream(logFilePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+	var logStreamWriter = new StreamWriter(logFileStream) { AutoFlush = true };
+	Console.SetOut(logStreamWriter);
+	Console.WriteLine();
+
 	if (string.IsNullOrWhiteSpace(runningIdeInstancePidString)) throw new InvalidOperationException("SharpIDE PID was not provided");
 	var runningIdeInstancePid = int.Parse(runningIdeInstancePidString);
 	var sharpIdeInstallDir = new DirectoryInfo(sharpIdeInstallPath!);
@@ -20,6 +27,7 @@ try
 	Console.WriteLine($"Update will be installed at: {sharpIdeInstallPath}");
 	Console.WriteLine($"Executable to run after update: {sharpIdeRunningExecutableFilePath}");
 	Console.WriteLine($"New Release: {newSharpIdeReleaseFilePath}");
+	Console.WriteLine($"Updater PID: {Environment.ProcessId}");
 
 	try
 	{
@@ -35,35 +43,70 @@ try
 	}
 
 	Console.WriteLine($"SharpIDE process exited, proceeding with update...");
+	var currentWorkingDirectory = Environment.CurrentDirectory;
 
-	var dirs = sharpIdeInstallDir.EnumerateDirectories().ToList();
-
-	if (dirs.Any(d => d.Name.Equals("GodotSharp", StringComparison.OrdinalIgnoreCase)) ||
-	    !dirs.Any(d => d.Name.StartsWith("data_SharpIDE", StringComparison.OrdinalIgnoreCase)))
+	if (OperatingSystem.IsMacOS())
 	{
-		Console.WriteLine("Install directory doesn't appear to be a published SharpIDE instance, aborting update!");
-		Console.WriteLine("Press any key to exit...");
-		Console.ReadKey();
-		return;
-	}
+		var sharpIdeAppDir = sharpIdeInstallDir;
+		while (sharpIdeAppDir is not null && sharpIdeAppDir.Extension != ".app")
+		{
+			sharpIdeAppDir = sharpIdeAppDir.Parent;
+		}
+		if (sharpIdeAppDir is null || sharpIdeAppDir.Name is not "SharpIDE.app") throw new InvalidOperationException("Could not find SharpIDE.app directory containing SharpIDE install directory");
+		Console.WriteLine($"MacOS: .app to be replaced: {sharpIdeAppDir.FullName}");
+		Console.WriteLine("Removing old version...");
+		var sharpIdeAppParentDirectory = sharpIdeAppDir.Parent ?? throw new InvalidOperationException("Could not find parent directory of SharpIDE.app");
+		sharpIdeAppDir.Delete(true);
 
-	Console.WriteLine("Removing old version...");
-
-	foreach (var fileSystemInfo in sharpIdeInstallDir.EnumerateFileSystemInfos())
-	{
-        if (fileSystemInfo is DirectoryInfo directoryInfo) directoryInfo.Delete(true); else fileSystemInfo.Delete();
-	}
-
-	Console.WriteLine("Copying new version...");
-
-	if (OperatingSystem.IsLinux())
-	{
-		await TarFile.ExtractToDirectoryAsync(newSharpIdeReleaseFilePath, sharpIdeInstallDir.FullName, false);
-	}
-	else if (OperatingSystem.IsWindows())
-	{
+		Console.WriteLine("Copying new version...");
 		using var archive = await ZipFile.OpenReadAsync(newSharpIdeReleaseFilePath);
-		archive.ExtractToDirectory(sharpIdeInstallDir.FullName);
+		await archive.ExtractToDirectoryAsync(sharpIdeAppParentDirectory.FullName);
+
+		Console.WriteLine("Macos: AdHoc code-signing...");
+		// On MacOS, Process.Start will attempt to resolved the supplied working directory, by getting the cwd of this process
+		// We technically deleted and replaced it, which means it blows up. Re-resolve the cwd.
+		Environment.CurrentDirectory = currentWorkingDirectory;
+		var psi = new ProcessStartInfo
+		{
+			FileName = "codesign",
+			ArgumentList = { "--force", "--deep", "--sign", "-", sharpIdeAppDir.FullName },
+			WorkingDirectory = currentWorkingDirectory,
+			RedirectStandardOutput = true,
+			RedirectStandardError = true,
+			UseShellExecute = false
+		};
+		var codeSignProcess = Process.Start(psi);
+		await codeSignProcess.WaitForExitAsync();
+	}
+	else
+	{
+		var dirs = sharpIdeInstallDir.EnumerateDirectories().ToList();
+
+		if (dirs.Any(d => d.Name.Equals("GodotSharp", StringComparison.OrdinalIgnoreCase)) ||
+		    !dirs.Any(d => d.Name.StartsWith("data_SharpIDE", StringComparison.OrdinalIgnoreCase)))
+		{
+			Console.WriteLine("Install directory doesn't appear to be a published SharpIDE instance, aborting update!");
+			return;
+		}
+
+		Console.WriteLine("Removing old version...");
+
+		foreach (var fileSystemInfo in sharpIdeInstallDir.EnumerateFileSystemInfos())
+		{
+			if (fileSystemInfo is DirectoryInfo directoryInfo) directoryInfo.Delete(true); else fileSystemInfo.Delete();
+		}
+
+		Console.WriteLine("Copying new version...");
+
+		if (OperatingSystem.IsLinux())
+		{
+			await TarFile.ExtractToDirectoryAsync(newSharpIdeReleaseFilePath, sharpIdeInstallDir.FullName, false);
+		}
+		else if (OperatingSystem.IsWindows())
+		{
+			using var archive = await ZipFile.OpenReadAsync(newSharpIdeReleaseFilePath);
+			await archive.ExtractToDirectoryAsync(sharpIdeInstallDir.FullName);
+		}
 	}
 
 	Console.WriteLine("Successfully updated, re-launching SharpIDE...");
@@ -73,8 +116,6 @@ catch (Exception ex)
 {
 	Console.WriteLine();
 	Console.WriteLine($"Updating SharpIDE Failed: {ex}");
-	Console.WriteLine("Press any key to exit...");
-	Console.ReadKey();
 	return;
 }
 
