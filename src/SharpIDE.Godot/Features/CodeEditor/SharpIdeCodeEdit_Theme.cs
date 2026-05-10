e5fb7d7a22f8bd00361b719249d0d3a08287e156
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-05-10 11:21:36 +1000

    Fix godot warning

--- src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit_Theme.cs
+++ src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit_Theme.cs
@@ -5,9 +5,6 @@ namespace SharpIDE.Godot.Features.CodeEditor;
 
 public partial class SharpIdeCodeEdit
 {
-    private static readonly StringName ThemeInfoStringName = "ThemeInfo";
-    private static readonly StringName IsLight1OrDark2StringName = "IsLight1OrDark2";
-
     private void UpdateEditorThemeForCurrentTheme()
     {
         var ideTheme = Singletons.AppState.IdeSettings.Theme;
@@ -23,7 +20,5 @@ public partial class SharpIdeCodeEdit
     private void UpdateEditorTheme(LightOrDarkTheme lightOrDarkTheme)
     {
         _syntaxHighlighter.UpdateThemeColorCache(lightOrDarkTheme);
-        SyntaxHighlighter = null;
-        SyntaxHighlighter = _syntaxHighlighter; // Reassign to trigger redraw
     }
 }
